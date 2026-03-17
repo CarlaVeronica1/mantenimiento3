4818bc8d1ee690ca53dcc80d6ce778f41d26263f
CarlaVeronica1 <163350931+CarlaVeronica1@users.noreply.github.com>
2026-03-17 10:16:24 -0600

    mensaje

--- api/units/createUnit.js
+++ api/units/createUnit.js
@@ -26,6 +26,6 @@ module.exports = async (req, res) => {
     res.status(201).json(result.rows[0]);
 
   } catch (error) {
-    res.status(500).json({ message: "Error servidor" });
+    res.status(500).json({ message: "Create Error servidor" });
   }
 };
