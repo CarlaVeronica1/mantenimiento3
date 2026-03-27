7eecc2491e7106ecb6b397b2237dc51a64cfe15c
CarlaVeronica1 <163350931+CarlaVeronica1@users.noreply.github.com>
2026-03-26 21:52:45 -0600

    UPDATE

--- api/units/updateUnit.js
+++ api/units/updateUnit.js
@@ -5,7 +5,7 @@ module.exports = async (req, res) => {
   
      //  CORS HEADERS (IMPORTANTE)
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS,UPDATE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
     // 👇 MANEJAR PREFLIGHT
